ee39280d6131c91ce2a50250f7ef59edddbac177
cae calcolari <eduardokae01@hotmail.com>
2026-03-08 12:42:04 -0300

    chore: jest / ty config

--- jest.config.cjs
+++ jest.config.cjs
@@ -5,7 +5,17 @@ const tsJestTransformCfg = createDefaultPreset().transform;
 /** @type {import("jest").Config} **/
 module.exports = {
   testEnvironment: "node",
+  preset: "ts-jest",
+  extensionsToTreatAsEsm: [".ts"],
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+  testMatch: ["**/__tests__/**/*.test.ts"],
   transform: {
     ...tsJestTransformCfg,
   },
+  collectCoverageFrom: [
+    "src/**/*.ts",
+    "!src/**/*.d.ts",
+  ],
 };
